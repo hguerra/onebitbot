5fe51d6e747b5a0e496a9f4c3cafe084d8bc8785
Heitor Carneiro <heitorgcarneiro@gmail.com>
2018-07-21 14:08:47 +0000

    Adding tests of all services

---
+++ spec/app_spec.rb
@@ -0,0 +1,11 @@
+require_relative './spec_helper.rb'
+require_relative '../app.rb'
+
+RSpec.describe App do
+  it 'get sinatra response' do
+    get '/'
+
+    expect(last_response).to be_ok
+    expect(last_response.body).to match('Hello world!')
+  end
+end

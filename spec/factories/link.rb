dc62274f33cfea46ae752c6cf47f1fe832f2f624
Heitor Carneiro <heitorgcarneiro@gmail.com>
2018-07-24 23:42:59 +0000

    Adding Link::ListService#call spec

--- spec/factories/link.rb
+++ spec/factories/link.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :link do
     link FFaker::Internet.http_url
+    company
   end
 end
--- spec/services/link/list_service_spec.rb
+++ spec/services/link/list_service_spec.rb
@@ -16,5 +16,35 @@ describe LinkModule::ListService do
       response = list_service.call
       expect(response).to eq('Nada encontrado')
     end
+
+    it 'With two links, find links' do
+      list_service = LinkModule::ListService.new({}, 'list')
+
+      link1 = create(:link, company: @company)
+      link2 = create(:link, company: @company)
+
+      response = list_service.call
+
+      expect(response).to eq(link1.link)
+      expect(response).to eq(link2.link)
+    end
+
+    it 'With invalid hashtag, return dont find message' do
+      list_service = LinkModule::ListService.new({ 'query': '' }, 'search_by_hashtag')
+
+      response = list_service.call
+      expect(response).to eq('Nada encontrado')
+    end
+
+    it 'With valid hashtag, find link in response' do
+      link = create(:link, company: @company)
+      hashtag = create(:hashtag, company: @company)
+      create(:link_hashtag, link: link, hashtag: hashtag)
+
+      list_service = LinkModule::ListService.new({ 'query': hashtag.name }, 'search_by_hashtag')
+
+      response = list_service.call
+      expect(response).to eq(link.link)
+    end
   end
 end
